c5b5f966a362464aaf0481b494c20cb60a6d7d7d
Chelsie <kexie18@gmail.com>
2024-02-28 15:07:37 -0500

    commit

--- rag.py
+++ rag.py
@@ -9,11 +9,11 @@ from operator import itemgetter
 import os 
 
 embeddings=ModelScopeEmbeddings(model_id='iic/nlp_corom_sentence-embedding_english-base')
-# embeddings=ModelScopeEmbeddings(model_id='sentence-transformers/all-MiniLM-L6-v2')
+
 
 
 vector_db=FAISS.load_local('Lecture4.faiss',embeddings)
-# vector_db=FAISS.load_local('Lecture4.faiss',embeddings)
+
 retriever=vector_db.as_retriever(search_kwargs={"k":5})
 
 chat=ChatOpenAI(
@@ -30,22 +30,6 @@ Question: {query}
 ''')
 full_chat_prompt=ChatPromptTemplate.from_messages([system_prompt,MessagesPlaceholder(variable_name="chat_history"),user_prompt])
 
-'''
-<|im_start|>system
-You are a helpful assistant.
-<|im_end|>
-...
-<|im_start|>user
-Answer the question based only on the following context:
-
-{context}
-
-Question: {query}
-<|im_end|>
-<|im_start|>assitant
-......
-<|im_end|>
-'''
 
 # Chat chain
 chat_chain={
